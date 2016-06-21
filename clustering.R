30cf7f355506796f9a3316409e825824f0003326
Simon Carrignon <simon.carrignon@gmail.com>
2016-06-21 16:36:32 +0200

    move data

--- clustering.R
+++ clustering.R
@@ -1,6 +1,6 @@
 library(dendextend)
 #Get the raw data
-rawdata=read.csv("Widmer et al_2012.xlsx",sep='\t')
+rawdata=read.csv("data/Widmer et al_2012.xlsx",sep='\t')
 
 #get a subset
 rawdata=rawdata[1:100,]
@@ -15,9 +15,10 @@ fungclust=hclust(dist(funghus))
 algclust=hclust(dist(algae))
 fungclust_dend=as.dendrogram(fungclust)
 algclust_dend=as.dendrogram(algclust)
-algclust_dendmod=color_branches(algclust_dend,k=10)
-fungclust_dendmod=color_branches(fungclust_dend,k=10)
-
-png("test.png",width=800,height=1200)
+algclust_dendmod=color_branches(algclust_dend,k=100)
+fungclust_dendmod=color_branches(fungclust_dend,k=100)
+unttangdend=untangle_step_rotate_1side(algclust_dendmod,fungclust_dendmod)
+png("test.png",width=1000,height=4200)
+#tanglegram(unttangdend[[1]],unttangdend[[2]])
 tanglegram(algclust_dendmod,fungclust_dendmod)
 dev.off()
