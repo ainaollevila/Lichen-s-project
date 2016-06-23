f8b02ba19c3680d0ff3f733ab58d473a2b3b40ec
Simon Carrignon <simon.carrignon@gmail.com>
2016-06-24 00:32:29 +0200

    few R modifs

--- clustering.R
+++ clustering.R
@@ -7,18 +7,35 @@ rawdata=rawdata[1:100,]
 rownames(rawdata)=rawdata[,1]
 
 #separate th microsat
-funghus=rawdata[,3:8]
-algae=rawdata[,8:17]
+funghus=rawdata[,3:11]
+algae=rawdata[,12:17]
+fdistmat=as.matrix(dist(funghus))
+adistmat=as.matrix(dist(algae))
+
+pdf("dist_mat.pdf")#,width=7,height=8)
+par(mfrow=c(2,1),mar=c(0,0,0,0))
+image(fdistmat)#,axis="n",main="funghi")
+image(adistmat)#,axis="n",,main="algae")
+dev.off()
+
+png("dist_mat.png",width=2000,height=4000)
+par(mfrow=c(2,1),mar=c(0,0,0,0))
+image(fdistmat)#,axis="n",main="funghi")
+image(adistmat)#,axis="n",,main="algae")
+dev.off()
 
 #compute distance and clust
 fungclust=hclust(dist(funghus))
 algclust=hclust(dist(algae))
+
 fungclust_dend=as.dendrogram(fungclust)
+
 algclust_dend=as.dendrogram(algclust)
 algclust_dendmod=color_branches(algclust_dend,k=100)
 fungclust_dendmod=color_branches(fungclust_dend,k=100)
-unttangdend=untangle_step_rotate_1side(algclust_dendmod,fungclust_dendmod)
-png("test.png",width=1000,height=4200)
+unttangdend=untangle_step_rotate_1side(algclust_dend,fungclust_dend)
+
+png("testuntang.png",width=800,height=1000,pointsize=17)
 #tanglegram(unttangdend[[1]],unttangdend[[2]])
-tanglegram(algclust_dendmod,fungclust_dendmod)
+tanglegram(algclust_dend,fungclust_dend)
 dev.off()
