f02de6adf07362ac9864aa3b21af1b9fd77d49da
Simon Carrignon <simon.carrignon@gmail.com>
2016-06-28 03:00:20 +0200

    implementation of hamming

--- clustering.R
+++ clustering.R
@@ -1,11 +1,38 @@
-if(require("scales")){library(scales)}
+
+
 library(dendextend)
+simil=function(d){
+	sum(apply(d,2,function(i){ if(diff(i) == 0) return(0) else return(1)}))
+}	
+
+
+
+computeDist=function(d){
+	res=matrix(nrow=nrow(d),ncol=nrow(d))
+	for(i in 1:(nrow(d)-1)){
+		for(j in (i+1):(nrow(d))){
+			res[i,j]=simil(d[c(i,j),])
+			res[j,i]=simil(d[c(i,j),])
+		}
+	}
+	return(as.dist(res))
+}
+
+cooccurenceMat=function(mat){
+	res=matrix(0,nrow=nrow(mat),ncol=ncol(mat))
+	for(i in 1:(nrow(mat)-1)){
+		for(j in (i+1):(ncol(d))){
+			if(mat[i,j]<1)
+				res[i,j]=mat[i,j]
+		}
+	}
+}
 #Get the raw data
 rawdata=read.csv("data/Widmer et al_2012.xlsx",sep='\t')
 rawdata=read.csv("data/DalGrande_et_al_2012.csv",sep='\t')
 #rawdata=dal
 #get a subset
-rawdata=rawdata[1:200,]
+rawdata=rawdata[1:100,]
 rownames(rawdata)=rawdata[,1]
 
 #separate th microsat
@@ -32,8 +59,11 @@ image(adistmat)#,axis="n",,main="algae")
 dev.off()
 
 #compute distance and clust
-fungclust=hclust(dist(funghus))
-algclust=hclust(dist(algae))
+fungdist=computeDist(funghus)
+algdist=computeDist(algae)
+
+fungclust=hclust(fungdist)
+algclust= hclust(algdist)
 
 fungclust_dend=as.dendrogram(fungclust)
 algclust_dend=as.dendrogram(algclust)
@@ -42,7 +72,16 @@ algclust_dendmod=color_branches(algclust_dend,k=100)
 fungclust_dendmod=color_branches(fungclust_dend,k=100)
 unttangdend=untangle_step_rotate_1side(algclust_dend,fungclust_dend)
 
-png("cutdown.png",width=800,height=1000,pointsize=17)
+png("dalgrande.png",width=800,height=1000,pointsize=17)
 #tanglegram(unttangdend[[1]],unttangdend[[2]])
-tanglegram(algclust_dend,fungclust_dend)
+tanglegram(algclust_dend,fungclust_dend,axes=F,main_left="Algae",main_right="Funghi")
 dev.off()
+
+
+
+#a=c(1,1,12,13,600)
+#b=c(1,-11,12,101,4)
+#te=rbind(a,b)
+#res=simil(te)
+#print(res)
+
