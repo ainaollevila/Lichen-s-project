1f50d3073e26fd924b45d4fac068f428bd4c4ed4
ainaollevila <aina.olle@gmail.com>
2016-09-07 21:02:41 +0200

    read salva data correct & some results

--- Network analysis/Nestedness and modularity (Matlab)/BiMat-master/automatizationECHOnew.m
+++ Network analysis/Nestedness and modularity (Matlab)/BiMat-master/automatizationECHOnew.m
@@ -7,10 +7,35 @@ string = sprintf('/Users/aina/Dropbox/Lichen-s-project/dev/data/matrix%d.txt',i)
 %A = csvread(string)
 A = importdata(string)
 %%
-for i=1:8
-string = sprintf('/Users/aina/Dropbox/Lichen-s-project/dev/data/m%d.txt',i)
-string
+
+path = '/Users/aina/Dropbox/Lichen-s-project/dev/data/Simon'
+%%
+myFolderInfo = dir ('/Users/aina/Dropbox/Lichen-s-project/dev/data/Simon/')
+
+
+
+myFolderInfo(3).name
+%%
+
+
+stringlist = sprintf('%s/fileslist.txt',path)
+diary(stringlist)
+diary on
+for i=4:196
+    i
+    myFolderInfo(i).name
+end
+diary off
+
+
+%%
+
+for i=4:196
+string = sprintf('%s/%s',path,myFolderInfo(i).name)
+%string = sprintf('%s/m%d.txt',path,i)
+
 A = importdata(string)
+
 %A = csvread(string,1,1)
 % size(A) 
 % rows = size(A,1)
@@ -23,7 +48,7 @@ A = importdata(string)
 %     end
 % end
 bp = Bipartite(A);
-string2 = sprintf('/Users/aina/Dropbox/Lichen-s-project/dev/data/mat%d_genprop.txt',i)
+string2 = sprintf('%s/mat%d_genprop.txt',path,i)
 diary(string2)
 bp.printer.PrintGeneralProperties();
 
@@ -37,7 +62,7 @@ diary on
 bp.printer.PrintStructureValues();
 diary off
 
-string_fig1 = sprintf('/Users/aina/Dropbox/Lichen-s-project/dev/data/mat%d_fig1.pdf',i)
+string_fig1 = sprintf('%s/mat%d_fig1.pdf',path,i)
 figure(1);
 %set(gcf,'Position',[0 72 1751 922]);
 bp.plotter.use_type_interaction = true;
@@ -49,7 +74,7 @@ print(string_fig1,'-dpdf')
 close
 
 figure(2);
-string_fig2 = sprintf('/Users/aina/Dropbox/Lichen-s-project/dev/data/mat%d_fig2.pdf',i)
+string_fig2 = sprintf('%s/mat%d_fig2.pdf',path,i)
 %set(gcf,'Position',[0+50 72 932 922]);
 bp.plotter.use_isocline = true;
 bp.plotter.isocline_color = 'red';
@@ -58,7 +83,7 @@ print(string_fig2,'-dpdf')
 close
 
 figure(3);
-string_fig3 = sprintf('/Users/aina/Dropbox/Lichen-s-project/dev/data/mat%d_fig3.pdf',i)
+string_fig3 = sprintf('%s/mat%d_fig3.pdf',path,i)
 %set(gcf,'Position',[0+100 72 1754 922]);
 subplot(1,2,1);
 bp.community = LPBrim(bp.matrix);
@@ -74,7 +99,7 @@ print(string_fig3,'-dpdf')
 close
 
 figure(4);
-string_fig4 = sprintf('/Users/aina/Dropbox/Lichen-s-project/dev/data/mat%d_fig4.pdf',i)
+string_fig4 = sprintf('%s/mat%d_fig4.pdf',path,i)
 
 %set(gcf,'Position',[0+150 72 1754 922]);
 bp.community = LeadingEigenvector(bp.matrix);
@@ -98,19 +123,20 @@ close
 
 figure(6);
 %set(gcf,'Position',[19+800 72 932 922]);
-string_fig6 = sprintf('/Users/aina/Dropbox/Lichen-s-project/dev/data/mat%d_fig6.pdf',i)
+string_fig6 = sprintf('%s/mat%d_fig6.pdf',path,i)
 bp.plotter.PlotModularGraph();
 print(string_fig6,'-dpdf')
 close
 
 %Statistics
-string_statistics = sprintf('/Users/aina/Dropbox/Lichen-s-project/dev/data/mat%d_Statistics.txt',i)
+string_statistics = sprintf('%s/mat%d_Statistics.txt',path,i)
 diary(string_statistics)
 diary off
 bp.statistics.DoCompleteAnalysis(100, @NullModels.EQUIPROBABLE);
 diary on
 bp.printer.PrintStructureStatistics();
-
+end 
+%%
 diary off
 bp.statistics.DoCompleteAnalysis(100, @NullModels.AVERAGE);
 diary on
@@ -132,4 +158,4 @@ diary on
 bp.printer.PrintStructureStatistics();
 diary off
 
-end
+%end
--- dev/RScripts/readSalvaData.R
+++ dev/RScripts/readSalvaData.R
@@ -1,27 +1,57 @@
-datafolder="~/Dropbox/Lichen-s-project/dev/data/"
-datafolder
-filename=paste(datafolder,"data_observed_pairs_t1.txt",sep="")
-filename=paste(datafolder,"edgelist.txt",sep="")
-tagsnum=7
+tagsnum=2048
 
-b <- c("mutualism","parasitism")
-ticks <-c(1500, 2000)
-ticks[1]
-for (i in 1:2){
-  for(j in 1:2){
-    filename=paste(b[i],"_",ticks[j],sep="")
-    filename
-  }
+#I KEEP IT, just in case we want to test specific matrices, as there are A LOT! if we want to test all of them, just 
+#R <- c("mutualism","parasitism")
+#C <- c("michaelis-menten","sigmoid")
+#ticks <-seq(from = 5000, to = 100000, by = 5000)
+#sex <- c(1, 5, 10, 50, 100)
+#repl <- c("A","B","C","D","E")
+#length(repl)
+#filename=paste(R[i],"_",C[j],"_",ticks[k],"ticks_",sex[l],"sexualreproduction_replicate",repl[m],sep="")
+
+#for (i in 1:2){
+#  for(j in 1:2){
+#    for(k in 1:length(ticks)){
+#      for(l in 1:length(sex)){
+#        for(m in 1:length(repl)){
+#          
+#        }
+#      }
+#    }
+#  }
+#}
+
+datafolder="../data/"
+i=1
+filename=paste(datafolder,"data_observed_pairs_t",i,".txt",sep="")
+
+for (i in 1:8){
+  i=1
+  filename=paste(datafolder,"data_observed_pairs_t",i,".txt",sep="")
+  m = edgelisttomatrix(filename,0)  #1 if you want the weighted matrix, 0 otherwise
+  mat = removingzeros(m) #removing the rows and cols = 0
+  fileout = paste(datafolder,"m",i,".txt",sep="")
+  write.table(mat, fileout, row.names=FALSE, col.names=FALSE)
 }
 
-filename=paste(datafolder,"edgelist.txt",sep="")
+for (l in list.files(datafolder,pattern="*reproduction*"))
+{
+  filename=paste(datafolder,l,sep="")
+  m = edgelisttomatrix(filename,0)  #1 if you want the weighted matrix, 0 otherwise
+  mat = removingzeros(m) #removing the rows and cols = 0
+  fileout=paste(datafolder,"Matrix_",l,sep="")
+  write.table(mat, fileout, row.names=FALSE, col.names=FALSE)
+}
+
 
 edgelisttomatrix <-function(filename,weighted){
-  el=read.table(filename)
+  #el=read.table(filename)
+  el=read.csv(filename,header = FALSE, sep = ",")
   m <-matrix(0,nrow=tagsnum,ncol=tagsnum)
   for (i in 1:nrow(el)){
     origin = el[i,1]    
     dest = el[i,2]
+    weighted = 0
     if (weighted == 1) {m[origin,dest] = m[origin,dest] + 1}
     else {m[origin,dest] = 1}
   }
@@ -30,13 +60,9 @@ edgelisttomatrix <-function(filename,weighted){
 
 removingzeros <-function(salva_matrix){
   bool_zero_rows<-rep(0,tagsnum)
-  bool_zero_rows
   length(bool_zero_rows)
   bool_zero_cols<-rep(0,tagsnum)
-  bool_zero_cols
   length(bool_zero_cols)
-  salva_matrix
-  
   for (i in 1:tagsnum)
   {
     bool = 1
@@ -75,8 +101,6 @@ removingzeros <-function(salva_matrix){
     }
   }
   
-  bool_zero_cols
-  bool_zero_rows
   count_cols = 0
   count_rows = 0
   for (i in 1:tagsnum){
@@ -84,16 +108,12 @@ removingzeros <-function(salva_matrix){
       count_cols = count_cols + 1
     }
   }
-  count_cols
   for (i in 1:tagsnum){
     if (bool_zero_rows[i] == 0){
       count_rows = count_rows + 1
     }
   }
-  count_rows
-  
   salva_corrected <- array(0, dim=c(count_rows,count_cols)) #puc ficar-ho manera nova que s??
-  salva_corrected
   ip=1
   jp=1
   for (i in 1:tagsnum){
@@ -111,9 +131,41 @@ removingzeros <-function(salva_matrix){
   return(salva_corrected)
 }
 
-m = edgelisttomatrix(filename,1)  #1 if you want the weighted matrix, 0 otherwise
-removingzeros(m) #removing the rows and cols = 0
 
 
+#SIMON METHOD!!! Infinitely more optimal
+
+for (l in list.files(datafolder,pattern="*reproduction*"))
+{
+  filename=paste(datafolder,l,sep="")
+  data_model=read.csv(filename,header=F)
+  colnames(data_model)=c("A","F","x","y")
+  data_model
+  M=cooccurenceModel(data_model)
+  fileout=paste(datafolder,"MatrixS_",l,sep="")
+  write.table(mat, fileout, row.names=FALSE, col.names=FALSE)
+}
+
+cooccurenceModel <- function(datas){
+  print(unique(datas[,1]))
+  res=matrix(0,nrow=length(unique(datas$A)),ncol=length(unique(datas$F)))
+  
+  rownames(res)=unique(datas$A)
+  colnames(res)=unique(datas$F)
+  
+  print (res)
+  for(i in 1:nrow(datas)){
+    popiMlgF=as.character(datas[i,1])
+    popiMlgA=as.character(datas[i,2])
+    res[popiMlgF,popiMlgA]=res[popiMlgF,popiMlgA]+1
+  }
+  res
+  return(res)
+  
+}
 
 
+#vec_files <- list.files(datafolder,pattern="*reproduction*")
+
+#filename=paste(datafolder,vec_files[60],sep="")
+#filename
