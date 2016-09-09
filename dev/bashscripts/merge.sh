c4cb64d9866286a0f32dda1e389bea4a86b97bb0
Simon <simon.carrignon@gmail.com>
2016-09-09 07:47:26 +0200

    fix merge.sh with head -n 27 to avoid reading the full matrix in files *_Statistics.txt

--- dev/bashscripts/merge.sh
+++ dev/bashscripts/merge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#TODO let the user choos input folder and ouptut file
+#TODO let the user choose input folder and ouptut file
 #
 #cat data/Results/MatrixS_mutualism_michaelis-menten_30000ticks_10sexualreproduction_replicateA.dat_genprop.txt | sed "s/\s\+/,/g"
 
@@ -8,13 +8,11 @@ foldername=../data/Results/
 
 echo "">$foldername/result.csv
 
-cat data/Results/MatrixS_mutualism_michaelis-menten_30000ticks_10sexualreproduction_replicateA.dat_genprop.txt |  sed "s/\s*//g" | awk 'BEGIN{FS=":"; varname="" ; varvalue=""}{if($2 != ""){varname=$1","varname; }}END{print varname "mutualism,ticks,sexproba,rep"}' > "$foldername"/concatenate_result.csv
+head -n 27 data/Results/MatrixS_mutualism_michaelis-menten_30000ticks_10sexualreproduction_replicateA.dat_genprop.txt |  sed "s/\s*//g" | awk 'BEGIN{FS=":"; varname="" ; varvalue=""}{if($2 != ""){varname=$1","varname; }}END{print varname "mutualism,ticks,sexproba,rep"}' > "$foldername"/concatenate_result.csv
 
-#for mutualism in michaelis-menten linear sigmoid;
-for mutualism in michaelis-menten sigmoid;
+for mutualism in michaelis-menten linear sigmoid;
 do
-    #for rep in A B C D E;
-    for rep in A;
+    for rep in A B C D E;
     do
 	for ticks in `seq 10000 5000 100000`;
 	do
@@ -24,7 +22,7 @@ do
 		file="$foldername"/"$filename"
 		if [ -f $file ];
 		then
-		    cat  $file | sed "s/\s*//g" | awk -v mut="$mutualism" -v ticks="$ticks" -v sp="$sexprob" -v rep="$rep" 'BEGIN{FS=":"; varname="" ; varvalue=""}{if($2 != ""){varname=$1","varname; varvalue=$2","varvalue}}END{print varvalue mut","ticks","sp","rep}' >> "$foldername"/concatenate_result.csv
+		    head -n 27  $file | sed "s/\s*//g" | awk -v mut="$mutualism" -v ticks="$ticks" -v sp="$sexprob" -v rep="$rep" 'BEGIN{FS=":"; varname="" ; varvalue=""}{if($2 != ""){varname=$1","varname; varvalue=$2","varvalue}}END{print varvalue mut","ticks","sp","rep}' >> "$foldername"/concatenate_result.csv
 		fi
 	    done
 	done
