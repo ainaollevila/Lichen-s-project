979f2274c30d5f25cbba50df1433880475f0297e
Simon <simon.carrignon@gmail.com>
2016-09-09 08:16:31 +0200

    change merge.sh to allow handle both genprop or Statistics files

--- dev/bashscripts/merge.sh
+++ dev/bashscripts/merge.sh
@@ -4,11 +4,19 @@
 #cat data/Results/MatrixS_mutualism_michaelis-menten_30000ticks_10sexualreproduction_replicateA.dat_genprop.txt | sed "s/\s\+/,/g"
 
 
+if [[ $1 != "genprop" && $1 != "Statistics"  ]];
+then
+    echo "usage : ./merge suffix"
+    echo " 	where suffix is 'genprop' or 'Statistics' "
+    exit 0
+fi
+
+suffix=$1
 foldername=../data/Results/
+outputname="$foldername"/concatenate_result_"$suffix".csv
 
-echo "">$foldername/result.csv
-
-head -n 27 data/Results/MatrixS_mutualism_michaelis-menten_30000ticks_10sexualreproduction_replicateA.dat_genprop.txt |  sed "s/\s*//g" | awk 'BEGIN{FS=":"; varname="" ; varvalue=""}{if($2 != ""){varname=$1","varname; }}END{print varname "mutualism,ticks,sexproba,rep"}' > "$foldername"/concatenate_result.csv
+echo "">$outputname
+cat "$foldername"/MatrixS_mutualism_michaelis-menten_10000ticks_10sexualreproduction_replicateA.dat_"$suffix".txt |  sed "s/\s*//g" | awk 'BEGIN{FS=":"; varname="" ; varvalue=""}{if($2 != ""){varname=$1","varname; }}END{print varname "mutualism,ticks,sexproba,rep"}' > "$outputname"
 
 for mutualism in michaelis-menten linear sigmoid;
 do
@@ -18,11 +26,11 @@ do
 	do
 	    for sexprob in 1 5 10 50 100; 
 	    do
-		filename=MatrixS_mutualism_"$mutualism"_"$ticks"ticks_"$sexprob"sexualreproduction_replicate"$rep".dat_genprop.txt
+		filename=MatrixS_mutualism_"$mutualism"_"$ticks"ticks_"$sexprob"sexualreproduction_replicate"$rep".dat_"$suffix".txt
 		file="$foldername"/"$filename"
 		if [ -f $file ];
 		then
-		    head -n 27  $file | sed "s/\s*//g" | awk -v mut="$mutualism" -v ticks="$ticks" -v sp="$sexprob" -v rep="$rep" 'BEGIN{FS=":"; varname="" ; varvalue=""}{if($2 != ""){varname=$1","varname; varvalue=$2","varvalue}}END{print varvalue mut","ticks","sp","rep}' >> "$foldername"/concatenate_result.csv
+		    cat  $file | sed "s/\s*//g" | awk -v mut="$mutualism" -v ticks="$ticks" -v sp="$sexprob" -v rep="$rep" 'BEGIN{FS=":"; varname="" ; varvalue=""}{if($1 == "string"){exit}if($2 != ""){varname=$1","varname; varvalue=$2","varvalue}}END{print varvalue mut","ticks","sp","rep}' >> "$outputname"
 		fi
 	    done
 	done
